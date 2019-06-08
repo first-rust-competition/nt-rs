c08ce954446f73d9e22161433ef4acd0e6177407
Redrield <redrield@gmail.com>
2019-06-07 23:21:19 -0400

    JS package name changing and published to npm

--- nt-ws/examples_js/list_entries.js
+++ nt-ws/examples_js/list_entries.js
@@ -1,4 +1,4 @@
-import * as nt from "nt-ws";
+import * as nt from "nt-client";
 
 nt.NetworkTables.connect("ws://127.0.0.1:1735", "nt-js").then(inst => {
     for(var [key, value] of inst.entries()) {
--- nt-ws/examples_js/simple_client.js
+++ nt-ws/examples_js/simple_client.js
@@ -1,4 +1,4 @@
-import * as nt from "nt-ws";
+import * as nt from "nt-client";
 
 nt.NetworkTables.connect("ws://127.0.0.1:1735", "nt-js").then(inst => {
     inst.create_entry(new nt.EntryData("/foo", 0, nt.EntryType.Double, 1.0)).then(id => {
